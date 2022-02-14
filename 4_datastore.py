6701a0bfe747fb2d17b361f6688e58ccbfeb3fe7
Philip Garcia <philip_garcia1@baylor.edu>
2022-02-14 14:46:25 -0600

    notes 2/14

--- 4_datastore.py
+++ 4_datastore.py
@@ -3,7 +3,7 @@
 # for each space represented as rows. Name your file 'retail_space.csv.
 
 
-'''
+"""
 Your final output should look like:
 
 room-number,use,sq-ft,price
@@ -13,37 +13,36 @@ room-number,use,sq-ft,price
 103,examination,125,150
 104,office,150,100
 
-'''
+"""
 
 
-
-
-datastore = { "medical":[
-      { "room-number": 100,
-        "use": "reception",
-        "sq-ft": 50,
-        "price": 75
-      },
-      { "room-number": 101,
-        "use": "waiting",
-        "sq-ft": 250,
-        "price": 75
-      },
-      { "room-number": 102,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 103,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 104,
-        "use": "office",
-        "sq-ft": 150,
-        "price": 100
-      }
-
-      ]
+datastore = {
+    "medical": [
+        {"room-number": 100, "use": "reception", "sq-ft": 50, "price": 75},
+        {"room-number": 101, "use": "waiting", "sq-ft": 250, "price": 75},
+        {"room-number": 102, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 103, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 104, "use": "office", "sq-ft": 150, "price": 100},
+    ]
 }
+
+outfile = open("retail_space.csv", "w")
+outfile.write("room-number,use,sq-ft,price\n")
+
+
+# print(datastore["medical"])
+
+for l in datastore["medical"]:
+    outfile.write(
+        str(l["room-number"])
+        + ","
+        + l["use"]
+        + ","
+        + str(l["sq-ft"])
+        + ","
+        + str(l["price"])
+        + "\n"
+    )
+
+
+outfile.close()
